85eb8e862ecf31248a2af149c5b8d04721bfb491
Stefan <stefan.bratanov@yahoo.com>
2016-12-21 23:17:25 +0200

    dont show sql logs, plus evicting objects already persisted for the session

--- src/main/java/database/PokerGameStore.java
+++ src/main/java/database/PokerGameStore.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import model.PokerGame;
 import model.PokerGameDetail;
 import org.hibernate.Session;
 
@@ -15,15 +16,16 @@ public class PokerGameStore {
     private final Session session;
 
     public synchronized void persistPokerGameDetail(PokerGameDetail detail) {
+        PokerGame pokerGame = detail.getPokerGame();
 
         session.beginTransaction();
-
         session.flush();
-        session.saveOrUpdate(detail.getPokerGame());
+        session.saveOrUpdate(pokerGame);
         session.saveOrUpdate(detail);
-        session.flush();
-
         session.getTransaction().commit();
+        session.flush();
+        session.evict(pokerGame);
+        session.evict(detail);
 
     }
 }
