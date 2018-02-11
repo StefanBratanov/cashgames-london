7815c465971504f9bd541716f2bed3f8f6f6ef33
Stefataa <stefan.bratanov@yahoo.com>
2018-02-11 19:43:23 +0000

    a little bit nicer output for last updated

--- src/main/resources/webapp/js/controller.js
+++ src/main/resources/webapp/js/controller.js
@@ -1,11 +1,10 @@
 angular.module('poker-games', [])
-    .controller('pokerGamesController', function($scope, $http) {
-        $http.get('/api/poker-venues').
-        then(function(response) {
+    .controller('pokerGamesController', function ($scope, $http) {
+        $http.get('/api/poker-venues').then(function (response) {
             var jsonData = response.data;
             var timeFormat = "YYYY-MM-DD HH:mm:ss"
             var now = moment().tz("Europe/London").format(timeFormat)
-            $scope.details = jsonData.map(function(detail) {
+            $scope.details = jsonData.map(function (detail) {
                 if (detail.latestUpdateTime !== null) {
                     var updatedAt = moment(detail.latestUpdateTime, timeFormat);
                     var hours = Math.abs(updatedAt.diff(now, 'hours'))
@@ -39,6 +38,12 @@ function readableLastUpdated(h, m) {
         mins = m - (h * 60)
     } else {
         mins = m;
+    }
+    if (h === 0 && m === 0) {
+        return 'Just now'
+    }
+    if (m % 60 === 0) {
+        return output.replace('and ', 'ago');
     }
     return output.concat(mins + ' mins ago');
 }
